303defdaf77ff11b2e3ec03f955daff469812d3f
jannik ostermayer <jannik.ostermayer@gmx.de>
2018-11-25 00:00:38 +0100

    change package import

--- app/src/main/java/com/ema/jannik/logbook/model/StageDao.kt
+++ app/src/main/java/com/ema/jannik/logbook/model/StageDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import com.ema.jannik.logbook.Model.Stage
+import com.ema.jannik.logbook.model.Stage
 
 @Dao
 interface StageDao {
