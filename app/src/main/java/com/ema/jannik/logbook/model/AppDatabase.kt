1038c6a5f7c35a69c828e7de40898d348f8efdc3
jannik ostermayer <jannik.ostermayer@gmx.de>
2018-11-25 11:43:20 +0100

    create new app icon

--- app/src/main/java/com/ema/jannik/logbook/model/AppDatabase.kt
+++ app/src/main/java/com/ema/jannik/logbook/model/AppDatabase.kt
@@ -4,11 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.ema.jannik.logbook.model.RouteDao
-import com.ema.jannik.logbook.model.StageDao
 
 /**
- * Created by Jannik on 11/24/2018.
+ * This abstract class represent the Database which contains the tables drive, route and stage.
  */
 
 @Database(entities = [Drive::class, Route::class, Stage::class], version = 1)
@@ -22,7 +20,7 @@ abstract class AppDatabase : RoomDatabase() {
 
         /**
          * Singleton Pattern, because each RoomDatabase instance is fairly expensive.
-         * create an instance of the RoomDatabase if the instance is null
+         * Create an instance of the RoomDatabase if the instance is null
          */
         fun getInstance(context: Context): AppDatabase? {
             if (instance == null){
