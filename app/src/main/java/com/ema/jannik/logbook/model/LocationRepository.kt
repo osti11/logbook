36dfca1c3af4c2f1adb6816a4932548a531a0669
jannik ostermayer <jannik.ostermayer@gmx.de>
2018-12-12 11:51:50 +0100

    modified add drive activity

--- app/src/main/java/com/ema/jannik/logbook/model/LocationRepository.kt
+++ app/src/main/java/com/ema/jannik/logbook/model/LocationRepository.kt
@@ -4,15 +4,21 @@ import android.app.Application
 import android.location.Location
 import android.os.AsyncTask
 import com.ema.jannik.logbook.model.location.Geocoding
+import com.ema.jannik.logbook.model.location.LocationUpdates
 
 class LocationRepository(application: Application) {
-    private var geocoding: Geocoding = Geocoding(application)
+    private val geocoding = Geocoding(application)
+    private val locationUpdates = LocationUpdates(application)
 
     /**
      * run the AsyncTask to get the address from the location
      */
     fun getAddress(location: Location){
         GetAddressAsyncTask(geocoding).execute(location)
+    }
+
+    fun getLocationUpdates(){
+        GetLocationUpdatesAsyncTask(locationUpdates)
     }
 
     companion object {
@@ -25,5 +31,13 @@ class LocationRepository(application: Application) {
                 return null
             }
         }
+
+        private class GetLocationUpdatesAsyncTask(private val locationUpdates: LocationUpdates): AsyncTask<Void, Void, Void>(){
+            override fun doInBackground(vararg params: Void?): Void? {
+                locationUpdates.startLocationUpdate()
+                return null
+            }
+        }
+
     }
 }
