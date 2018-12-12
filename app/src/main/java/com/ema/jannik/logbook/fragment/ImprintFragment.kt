9c917252fc3bc9b03e73e08aa9a07c3fc660312b
jannik ostermayer <jannik.ostermayer@gmx.de>
2018-12-12 11:41:47 +0100

    modified build gradle

--- app/src/main/java/com/ema/jannik/logbook/fragment/ImprintFragment.kt
+++ app/src/main/java/com/ema/jannik/logbook/fragment/ImprintFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ema.jannik.logbook.R
 
+
 class ImprintFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_imprint, container, false)
