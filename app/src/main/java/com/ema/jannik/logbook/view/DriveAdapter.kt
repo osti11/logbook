ffb0e6c02eae6012bdf456b4536a8777a69884e0
jannik ostermayer <jannik.ostermayer@gmx.de>
2018-12-18 22:16:10 +0100

    DriveAdapter get layout from settings

--- app/src/main/java/com/ema/jannik/logbook/view/DriveAdapter.kt
+++ app/src/main/java/com/ema/jannik/logbook/view/DriveAdapter.kt
@@ -1,5 +1,11 @@
 package com.ema.jannik.logbook.view
 
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+import android.provider.Settings.Global.getString
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,15 +13,16 @@ import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.ema.jannik.logbook.R
+import com.ema.jannik.logbook.fragment.SettingFragment
+import com.ema.jannik.logbook.helper.Utils
 import com.ema.jannik.logbook.model.database.Drive
-import android.provider.ContactsContract.CommonDataKinds.Note
-import com.ema.jannik.logbook.Utils
 
 
 /**
  * This Adapter is responsible for the RecyclerView of the MainActivity
+ * @property activity To access the shared preferences. This activity must be the MainActivity.
  */
-class DriveAdapter : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
+class DriveAdapter(val activity: Activity) : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
 
     private var listener: DriveAdapter.OnItemClickListener? = null
 
@@ -33,11 +40,30 @@ class DriveAdapter : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
 
     override fun onBindViewHolder(holder: DriveHolder, position: Int) {
         val currentDrive: Drive = drives.get(position)
-        holder.distance.setText(currentDrive.mileageStart.toString())   //TODO aktualisiern db
-        holder.purpose.setText(currentDrive.start.address)    //TODO variabel gestalten
-        holder.date.setText(currentDrive.start_timestamp.toString()) //TODO aktualisiern db
-        holder.duration.setText(currentDrive.duration.toString())
-
+        holder.upperRightCorner.text = getDriveProperty(
+            getSettingsLayout(
+                1
+            ),
+            currentDrive
+        )
+        holder.upperLeftCorner.text = getDriveProperty(
+            getSettingsLayout(
+                2
+            ),
+            currentDrive
+        )
+            holder.lowerLeftCorner.text = getDriveProperty(
+                getSettingsLayout(
+                    3
+                ),
+                currentDrive
+            )
+            holder.lowerRightCorner.text = getDriveProperty(
+                getSettingsLayout(
+                    4
+                ),
+                currentDrive
+            )
         val category = Utils.getCategoryDrawableId(currentDrive.category)
         if (category != 0) {    //category == 0 -> not found
             holder.icon.setImageResource(category)
@@ -53,17 +79,17 @@ class DriveAdapter : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
 
     inner class DriveHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val icon: AppCompatImageView
-        val distance: TextView
-        val purpose: TextView
-        val date: TextView
-        val duration: TextView
+        val upperRightCorner: TextView
+        val upperLeftCorner: TextView
+        val lowerLeftCorner: TextView
+        val lowerRightCorner: TextView
 
         init {
             icon = itemView.findViewById(R.id.item_icon)
-            distance = itemView.findViewById(R.id.text_view_distance)
-            purpose = itemView.findViewById(R.id.a)
-            date = itemView.findViewById(R.id.date)
-            duration = itemView.findViewById(R.id.duration)
+            upperRightCorner = itemView.findViewById(R.id.textView_upperRightCorner)
+            upperLeftCorner = itemView.findViewById(R.id.textView_upperLeftCorner)
+            lowerLeftCorner = itemView.findViewById(R.id.textView_lowerLeftCorner)
+            lowerRightCorner = itemView.findViewById(R.id.textView_lowerRightCorner)
 
             itemView.setOnClickListener(object : View.OnClickListener {
                 /**
@@ -72,7 +98,7 @@ class DriveAdapter : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
                  * @param v The view that was clicked.
                  */
                 override fun onClick(v: View?) {
-                    val position = getAdapterPosition ()
+                    val position = getAdapterPosition()
                     if (listener != null && position != RecyclerView.NO_POSITION) {
                         listener!!.onItemClick(drives.get(position))
                     }
@@ -82,14 +108,72 @@ class DriveAdapter : RecyclerView.Adapter<DriveAdapter.DriveHolder>() {
             })
         }
     }
-        /**
-         * inteface for onItemClickListener
-         */
-        interface OnItemClickListener {
-            fun onItemClick(drive: Drive)
+
+    /**
+     * inteface for onItemClickListener
+     */
+    interface OnItemClickListener {
+        fun onItemClick(drive: Drive)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    fun getSettingsLayout(position: Int): String {
+        Log.i("SHAREDPREFERENCES", "access")
+        val sharedPreferences = activity.getSharedPreferences(
+            SettingFragment.SHARED_PREFERENCES, //name of shared preferences
+            Context.MODE_PRIVATE                //just this application can change the preferences
+        )
+        Log.i("SHAREDPREFERENCES", "when")
+        when (position) {
+            1 -> return sharedPreferences.getString(
+                SettingFragment.LAYOUT_ONE,
+                ""
+            )!!
+            2 -> return sharedPreferences.getString(
+                SettingFragment.LAYOUT_TWO,
+                ""
+            )!!
+            3 -> return sharedPreferences.getString(
+                SettingFragment.LAYOUT_THREE,
+                ""
+            )!!
+            4 -> return sharedPreferences.getString(
+                SettingFragment.LAYOUT_FOUR,
+                ""
+            )!!
+        }
+        return ""
+    }
+
+    /**
+     * This function get a string, a drive object and find the suitable property in the drive class.
+     * @param property use getSettingsLayout() to get this string.
+     * @param drive instanced drive object
+     * @return value of the suitable property. When nothing found return ""
+     */
+    fun getDriveProperty(property: String, drive: Drive): String {  //TODO retrurn null?
+        var x = 1
+        val array = activity.getResources().getStringArray(R.array.spinner_layout)
+        //iterate spinner_layout array
+        for(s in array){
+            if(s == property)
+                break
+            x++
         }
 
-        fun setOnItemClickListener(listener: OnItemClickListener) {
-           this.listener = listener
+        when (x) {
+            1 -> return drive.purpose
+            2 -> return drive.duration.toString()
+            3 -> return drive.start_timestamp.toString()
+            4 -> return drive.destination_timestamp.toString()
+            5 -> return drive.mileageStart.toString()
+            6 -> return drive.mileageDestination.toString()
+            7 -> return drive.start.address
+            8 -> return drive.destination.address
         }
+        return ""
     }
+}
