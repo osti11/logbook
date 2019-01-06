c2de2f2bfe894751b24873b368e69045d4a2df1a
jannik ostermayer <jannik.ostermayer@gmx.de>
2019-01-06 18:12:42 +0100

    update documentation

---
+++ app/src/main/java/com/ema/jannik/logbook/activity/EditDriveActivity.kt
@@ -0,0 +1,11 @@
+package com.ema.jannik.logbook.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ema/jannik/logbook/model/EditDriveRepository.kt
@@ -0,0 +1,2 @@
+package com.ema.jannik.logbook.model
+
