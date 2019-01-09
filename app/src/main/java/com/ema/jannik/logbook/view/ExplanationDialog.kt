3a305d98839800e2a78431b981e44838565d9157
jannik ostermayer <jannik.ostermayer@gmx.de>
2019-01-09 08:21:38 +0100

    create alert dialog for explanations

--- app/src/main/java/com/ema/jannik/logbook/view/ExplanationDialog.kt
+++ app/src/main/java/com/ema/jannik/logbook/view/ExplanationDialog.kt
@@ -8,11 +8,38 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatDialogFragment
 import com.ema.jannik.logbook.R
 
+/**
+ * This class open an Dialog that explain the settings.
+ * @param string This is the id of an entry in the string.xml file which will set as message.
+ */
 @SuppressLint("ValidFragment")
-class ExplanationDialog(val string: Int) : AppCompatDialogFragment(), DialogInterface.OnClickListener {
+class ExplanationDialogSettings(val string: Int) : AppCompatDialogFragment(), DialogInterface.OnClickListener {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val alertDialogBuilder = AlertDialog.Builder(activity)
-        alertDialogBuilder.setTitle(R.string.alertDialog_title)
+        alertDialogBuilder.setTitle(R.string.alertDialog_titleSettings)
+            .setMessage(string)
+
+        alertDialogBuilder.setPositiveButton("OK", this)
+
+        return alertDialogBuilder.create()
+    }
+
+    /**
+     * close dialog when ok 'OK' clicked.
+     */
+    override fun onClick(dialog: DialogInterface?, which: Int) {}
+}
+
+/**
+ * Diese Klasse wird von der AddDriveActivity verwendet, wenn falsche werte
+ * abgegeben werden um den Benutzer darauf hinzuweisen.
+ * @param string Dieser String wird als message des alertDialog gesetzt.
+ */
+@SuppressLint("ValidFragment")
+class ExplanationDialogAddDrive(val string: String) : AppCompatDialogFragment(), DialogInterface.OnClickListener {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        alertDialogBuilder.setTitle(R.string.alertDialog_titleAddDrive)
             .setMessage(string)
 
         alertDialogBuilder.setPositiveButton("OK", this)
