7aa038baac7d72a82f01cf8251d01284bc449fb5
jannik ostermayer <jannik.ostermayer@gmx.de>
2019-01-09 08:18:07 +0100

    implement converter from calendar to long to save the entries in th db

--- app/src/main/java/com/ema/jannik/logbook/model/database/Converts.kt
+++ app/src/main/java/com/ema/jannik/logbook/model/database/Converts.kt
@@ -3,11 +3,25 @@ package com.ema.jannik.logbook.model.database
 import androidx.room.TypeConverter
 import java.sql.Date
 import java.sql.Time
+import java.util.*
 
 /**
  * This class converts unstorable data types into data types that can be stored in the database
  */
 class Converts {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Calendar? {    //TODO stimmt das
+        val cal = Calendar.getInstance()
+        cal.timeInMillis = value!!
+        return cal
+    }
+
+    @TypeConverter
+    fun CalendarToTimestamp(calendar: Calendar?): Long? {
+        return calendar?.timeInMillis
+    }
+
+    /*
     companion object {
         /**
          * convert Timestamp into Date, to save it in the database
@@ -28,4 +42,5 @@ class Converts {
             return date?.time
         }
     }
+    */
 }
