73871c4870e5d3054eab155605b65e3c58f7f83f
jannik ostermayer <jannik.ostermayer@gmx.de>
2019-01-09 07:58:04 +0100

    AddDriveRepository run an async task to insert an entry in the db

--- app/src/main/java/com/ema/jannik/logbook/model/AddDriveRepository.kt
+++ app/src/main/java/com/ema/jannik/logbook/model/AddDriveRepository.kt
@@ -1,4 +1,33 @@
-package com.ema.jannik.logbook.model.location
+package com.ema.jannik.logbook.model
 
-class AddDriveRepository {
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+import com.ema.jannik.logbook.model.database.AppDatabase
+import com.ema.jannik.logbook.model.database.Drive
+import com.ema.jannik.logbook.model.database.DriveDao
+
+class AddDriveRepository(application: Application) {
+    private var driveDao: DriveDao
+
+    init {
+        val database: AppDatabase = AppDatabase.getInstance(application.applicationContext)!!
+        driveDao = database.driveDao()
+    }
+
+    /**
+     * run an async task to insert an entry into table "drive"
+     */
+    fun insert(drive: Drive){
+        InsertDriveAsyncTask(driveDao).execute(drive)
+    }
+
+    companion object {
+        private class InsertDriveAsyncTask(private val driveDao: DriveDao) : AsyncTask<Drive, Void, Void>(){
+            override fun doInBackground(vararg params: Drive): Void? {
+                driveDao.insert(params[0])
+                return null
+            }
+        }
+    }
 }
